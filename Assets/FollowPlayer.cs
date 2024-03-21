d4e664fb75018c7c01d53b6b8ae1883be7625801
1Ricardo5 <114586483+1Ricardo5@users.noreply.github.com>
2024-03-21 12:38:46 -0400

    work

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -7,9 +7,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
    public GameObject player;
-   private Vector3 offset = new Vector3(0, 5, -7);
-    
-    
+   private Vector3 offset = new Vector3(0, 4, 3);
+   private Vector3 offset2 = new Vector3(0, 5, -10);
+   
+    bool Pov = true;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,30 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-      transform.position = player.transform.position + offset;
+      
+      if(Input.GetKeyDown(KeyCode.V) && Pov == true)
+      {
+        Debug.Log("check2");
+        transform.position = player.transform.position + offset;
+        Pov = false;
+      }
+
+      else if (Input.GetKeyDown(KeyCode.V) && Pov == false)
+      {
+        Debug.Log("check");
+        transform.position = player.transform.position + offset2;
+        Pov = true;
+      }
     }
+   
+    
+    
+
+
+
+
+
+
+
+
 }
